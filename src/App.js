e5c05c4773a6de58ab387311207743c11accdf99
Arvind-mocha <64320021+arvind-mocha@users.noreply.github.com>
2021-06-02 10:36:30 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Bye there!</p>
+        <p>Hello There</p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          I love React
+          Learn React
         </a>
       </header>
     </div>
